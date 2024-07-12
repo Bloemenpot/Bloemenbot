2d2336215d8639663200f3745740f4aaeae2450d
Bloemenpot <mjssmith03@gmail.com>
2024-07-13 01:35:40 +0200

    Working Connection

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-const {} = recuire('discord.js');
+const {Client, IntentsBitField } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config({path: '.env'});
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+client.login(process.env.DCTOKEN);
+console.log("Logged in");
